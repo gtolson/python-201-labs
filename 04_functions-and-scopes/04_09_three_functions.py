cdeb25301bce000f5d1397e59457d7ea326852ca
Gary <gary@gtolson.com>
2024-03-31 14:56:08 -0500

    04 labs rest of

--- 04_functions-and-scopes/04_09_three_functions.py
+++ 04_functions-and-scopes/04_09_three_functions.py
@@ -5,3 +5,28 @@
 # in a circular way.
 
 
+
+def name_entry():
+    name_in = input("What is the name of the person you want to address?: ")
+    return name_in
+
+def greeting(name):
+    name  = name_entry()
+    full_greeting = (f"\nHi {name}, welcome to the Thunderdome, \n\n")
+    return full_greeting
+
+def body(greeting):
+    greeting = greeting(name_entry)
+    letter_so_far = ""
+    main_body = (f'You have earned the right to compete in a battle of wills against the worlds greatest warriors. \n\n')
+    letter_so_far = greeting + main_body
+    return letter_so_far
+
+def letter(body):
+    close = (f'Best of luck to you brave warrior! \n')
+    body = body(greeting)
+    full_letter = body + close
+    return full_letter
+
+
+print(letter(body))
--- 04_functions-and-scopes/04_11_your_realtor.py
+++ 04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,19 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+
+
+#home_info = 'Address="1514 Brompton Ln",City="Crystal Lake",State="IL",Zip="60014",Bedrooms="2",Total_baths="2",Full_baths="1",Square_ft="1556",Acres=".2",Year_built="2007",Heating="Yes",Cooling="Yes",hoa="No"'
+
+
+
+def listing(**home_info):
+    listing_items = []
+    print("Here are the details for your listing:")
+    for k,v in home_info.items():
+        print(f"{k}: {v:>20}")
+        
+
+        
+listing(Address="1514 Brompton Ln",City="Crystal Lake",State="IL",Zip="60014",Bedrooms="2",Total_baths="2",Full_baths="1",Square_ft="1556",Acres=".2",Year_built="2007",Heating="Yes",Cooling="Yes",hoa="No")
+
